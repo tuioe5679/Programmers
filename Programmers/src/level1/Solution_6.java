b4b6ee48a1d2f60f7ab8680bf038c34e00e7148f
tuioe5679 <qodlgks123@naver.com>
2022-06-28 20:43:12 +0900

    Add level_1 1~6

---
+++ Programmers/src/level1/Solution_6.java
@@ -0,0 +1,15 @@
+package level1;
+
+/*정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.*/
+
+public class Solution_6 {
+	public double solution(int[] arr) {
+		double answer = 0;
+		int count = 0;
+		for (int i : arr) {
+			answer += i;
+			count++;
+		}
+		return answer / count;
+	}
+}
