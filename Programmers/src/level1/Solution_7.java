89517649bb78177f45e794c998e7fb0a277a2532
tuioe <qodlgks123@naver.com>
2022-07-21 16:05:59 +0900

    Add level_1 7

--- Programmers/src/level1/Solution_7.java
+++ Programmers/src/level1/Solution_7.java
@@ -1,10 +1,31 @@
 package level1;
 
+//콜라츠 추측
 public class Solution_7 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int solution(long num) {
+		long value = 0;
+		int answer = 0;
+		if (num % 2 == 0) {
+			value = num / 2;
+			answer++;
+		} else {
+			value = (num * 3) + 1;
+			answer++;
+		}
+		while (value != 1) {
+			if (value % 2 == 0) {
+				value = value / 2;
+				answer++;
+			} else {
+				value = (value * 3) + 1;
+				answer++;
+			}
+		}
+		if (num == 1) {
+			return 0;
+		} else if (answer >= 500) {
+			return -1;
+		}
+		return answer;
 	}
-
 }
