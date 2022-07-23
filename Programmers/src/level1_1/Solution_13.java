30bd23a2db3448bf2daead8cb26efb183a98d25a
tuioe5679 <qodlgks123@naver.com>
2022-07-23 23:06:00 +0900

    자연수 뒤집어 배열로 만들기

--- Programmers/src/level1_1/Solution_13.java
+++ Programmers/src/level1_1/Solution_13.java
@@ -3,5 +3,17 @@ package level1_1;
 /*
 자연수 뒤집어 배열로 만들기*/
 public class Solution_13 {
-
+	public static int[] solution(long n) {
+        String a = String.valueOf(n);
+        String b[] = a.split("");
+        
+        int[] answer = new int[b.length];
+        int value = b.length-1;
+        
+        for(int i=0;i<b.length;i++) {
+        	answer[value] = Integer.parseInt(b[i]);
+        	value--;
+        }
+        return answer;
+    }
 }
