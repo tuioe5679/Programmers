48a45b4848a735ece0cd3c662b61986d155953d7
tuioe5679 <qodlgks123@naver.com>
2022-07-23 21:26:38 +0900

    짝수와 홀수

---
+++ Programmers/src/level1/Solution_9.java
@@ -0,0 +1,16 @@
+package level1;
+
+/*
+짝수와 홀수*/
+public class Solution_9 {
+	public String solution(int num) {
+        String answer = "";
+        if(num%2==0){
+            answer="Even";
+        }
+        else{
+            answer="Odd";
+        }
+        return answer;
+    }
+}
