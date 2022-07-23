7ed24e8a4b27b115688bacaf3fbb140f1d776db1
tuioe5679 <qodlgks123@naver.com>
2022-07-23 22:00:23 +0900

    정수 제곱근 판별

--- Programmers/src/level1_1/Solution_11.java
+++ Programmers/src/level1_1/Solution_11.java
@@ -3,5 +3,8 @@ package level1_1;
 /*
 정수 제곱근 판별*/
 public class Solution_11 {
-
+	static long solution(long n) {
+		double answer =  Math.sqrt(n);
+        return (long) (answer % 1 ==0 ? Math.pow(Math.sqrt(n)+1,2) : -1);
+	}
 }
