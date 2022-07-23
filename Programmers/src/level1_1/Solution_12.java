18b85de2b3fb083a33963f2ccdf964626dab9d17
tuioe5679 <qodlgks123@naver.com>
2022-07-23 22:28:29 +0900

    정수 내림차순으로 배치하기

--- Programmers/src/level1_1/Solution_12.java
+++ Programmers/src/level1_1/Solution_12.java
@@ -5,22 +5,15 @@ import java.util.Arrays;
 /*
 정수 내림차순으로 배치하기*/
 public class Solution_12 {
-	public static void main(String[] args) {
-		solution(1234345345);
-	}
-
 	public static long solution(long n) {
 		long answer = 0;
 		String a = String.valueOf(n);
 		String b[] = a.split("");
 		Arrays.sort(b);
 		String c = "";
-		for(int i=b.length-2;i>=0;i--) {
+		for (int i = b.length - 1; i >= 0; i--) {
 			c += b[i];
 		}
-		
-		answer = Integer.parseInt(c);
-		System.out.println(answer);
-		return answer;
+		return Long.parseLong(c);
 	}
 }
