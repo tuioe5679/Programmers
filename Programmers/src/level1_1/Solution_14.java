7628a55421eade811d9e8b8d02a160422e85a3f4
tuioe5679 <qodlgks123@naver.com>
2022-07-24 16:46:28 +0900

    자릿수 더하기

--- Programmers/src/level1_1/Solution_14.java
+++ Programmers/src/level1_1/Solution_14.java
@@ -3,5 +3,12 @@ package level1_1;
 /*
 자릿수 더하기*/
 public class Solution_14 {
-
+	public int solution(int n) {
+        int answer = 0;
+        while(n!=0) {
+        	answer += n%10;
+        	n=n/10;
+        }
+        return answer;
+    }
 }
